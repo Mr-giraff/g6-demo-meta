714d9ae0bac31dff52ad37b54139fc5689aca682
tianzejun <tianzejun@startdt.com>
2021-10-08 14:51:06 +0800

    build: webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   lintOnSave: false,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"))
+      .set("views", resolve("src/views"))
+      .set("layout", resolve("src/layout"))
+      .set("base", resolve("src/base"))
+      .set("static", resolve("src/static"))
+      .set("utils", resolve("src/utils"))
+      .set("styles", resolve("src/styles"));
+
+    config.devtool("source-map");
+  },
 };
